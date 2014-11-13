e7ab4b2706cf3666a54e3067b8bee05c911eb424
wuwoot9863 <wuwoot9863@hotmail.com>
2014-11-13 13:28:04 -0500

    Update fallingObject.cpp

--- fallingObject.cpp
+++ fallingObject.cpp
@@ -1,7 +1,15 @@
 #include "fallingObject.h"
 #include <math.h>
 
-
+/*fallingObject 
+ *
+ *Constructor that creates an object that assigns constants to the object
+ *
+ *@param mass
+ *@param surfaceArea
+ *@param bungeeSpringConstant
+ *@param bungeeUnstretchedLength
+ */
 fallingObject::fallingObject(float mass, float surfaceArea, float bungeeSpringConstant, float bungeeUnstretchedLength){
 	this->mass = mass;
 	this->surfaceArea = surfaceArea;
@@ -9,36 +17,81 @@ fallingObject::fallingObject(float mass, float surfaceArea, float bungeeSpringCo
 	this->bungeeUnstretchedLength = bungeeUnstretchedLength;
 }
 
-
+/*getMass
+ *
+ *Method that returns the mass of the object
+ *
+ *@return mass The mass of the object
+ */
 float fallingObject::getMass(){
 	return mass;
 }
 
+/*getSurfaceArea
+*
+*Method that returns the surface area of the object
+*
+*@return surfaceArea The surface area of the object
+*/
 float fallingObject::getSurfaceArea(){
 	return surfaceArea;
 }
 
+/*getBungeeSpringConstant
+*
+*Method that returns the spring constant of the bungee cord
+*
+*@return bungeeSpringConstant The spring constant of the bungee cord
+*/
 float fallingObject::getBungeeSpringConstant(){
 	return bungeeSpringConstant;
 }
 
+/*getBungeeUnstretchedLength
+*
+*Method that returns the length of the bungee cord
+*
+*@return bungeeUnstretchedLength The length of the bungee cord
+*/
 float fallingObject::getBungeeUnstretchedLength(){
 	return bungeeUnstretchedLength;
 }
 
+/*getFallDistance
+*
+*Method that returns the falling distance of the object
+*
+*@return fallDistance The falling distance of the object
+*/
 float fallingObject::getFallDistance(){
 	return fallDistance;
 }
 
+/*getVelocity
+*
+*Method that returns the velocity of the object
+*
+*@return velocity The velocity of the object
+*/
 float fallingObject::getVelocity(){
 	return velocity;
 }
 
+/*getAcceleration
+*
+*Method that returns the acceleration of the object
+*
+*@return acceleration The acceleration of the object
+*/
 float fallingObject::getAcceleration(){
 	return acceleration;
 }
 
 
+/*simulateTimeStep
+*
+*Method that simulates the bungee jump of Julie-Ann if she were to take it
+*/
 void fallingObject::simulateTimeStep(float deltaT){
 	this->deltaT = deltaT;
 
