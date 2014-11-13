5c68d2f91507810b5d1a442536ab6774aba8845c
wuwoot9863 <wuwoot9863@hotmail.com>
2014-11-13 13:27:14 -0500

    Update fallingObject.h

--- fallingObject.h
+++ fallingObject.h
@@ -1,21 +1,21 @@
 class fallingObject
 {
 private:
-	float mass;
-	float surfaceArea;
-	float bungeeSpringConstant;
-	float bungeeUnstretchedLength;
-	float fallDistance = 0.0f;
-	float velocity = 0.0f;
-	float acceleration = 0.0f;
-	float deltaT;
-	float deltaV;
-	float deltaD;
-	float Ffriction;
-	float Fspring;
-	float Fweight;
-	float Ftotal;
-	float g = 9.81f;
+	float mass; //Mass of the object
+	float surfaceArea; //Surface area of the object
+	float bungeeSpringConstant; //Spring constant of the bungee cord
+	float bungeeUnstretchedLength; //Length of the bungee cord
+	float fallDistance = 0.0f; //Falling distance of the object
+	float velocity = 0.0f; //Velocity of the object
+	float acceleration = 0.0f; //Acceleration of the object
+	float deltaT; //Time increment change as the object is in motion
+	float deltaV; //Change in velocity at a time increment
+	float deltaD; //Change in distance at a time increment
+	float Ffriction; //Force of air friction acting on the object
+	float Fspring; //Force of the spring acting on the object
+	float Fweight; //Force of gratvity acting on the object
+	float Ftotal; //Net force acting on the object
+	float g = 9.81f; //Magnitude of gravity
 public:
 	virtual float getMass();
 	virtual float getSurfaceArea();
